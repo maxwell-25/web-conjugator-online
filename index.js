28bd2f8ee8dafb215b50c00e6bc6dad5da821fd4
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-03-16 08:36:40 +0000

    javascript development

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.getElementById("yo-ending").innerText = "-o"
+document.getElementById("yo").style.color="red";
+document.getElementById("yo-ex").innerText="habl-";
+
+function showAnswer(){
+    document.getElementById("yo-end").innerText="-o";
+    document.getElementById("yo-ex").innerText="hablo";
+}
