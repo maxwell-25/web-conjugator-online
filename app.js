0eb94b914cf66f9ba2f462cfeaa2b03255fec499
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-03-16 19:10:20 +0000

    index.js changed to app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById("yo-per").style.color="red";
+document.getElementById("yo-end").innerHTML="?"
+document.getElementById("yo-end").style.color="red";
+document.getElementById("yo-ex").innerHTML="habl-";
+document.getElementById("yo-ex").style.color="red";
+
+function showAnswer(){
+    document.getElementById("yo-per").style.color="green";
+    document.getElementById("yo-end").innerHTML ="-o";
+    document.getElementById("yo-end").style.color="green";
+    document.getElementById("yo-ex").innerHTML="hablo";
+    document.getElementById("yo-ex").style.color="green";
+}
--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("yo").style.color="red";
-document.getElementById("yo-ex").innerText="habl-";
-
-function showAnswer(){
-    document.getElementById("yo-end").innerText="-o";
-    document.getElementById("yo-ex").innerText="hablo";
-}
