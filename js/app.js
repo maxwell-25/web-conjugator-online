047bec14864ff29e04d8e2a50d2063df8d39a4f0
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-03-16 21:09:50 +0000

    files reorganised

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.getElementById("yo-ex").style.color="red";
 
 function showAnswer(){
     document.getElementById("yo-per").style.color="green";
-    document.getElementById("yo-end").innerHTML ="-o";
+    document.getElementById("yo-end").innerHTML="-o"
     document.getElementById("yo-end").style.color="green";
     document.getElementById("yo-ex").innerHTML="hablo";
     document.getElementById("yo-ex").style.color="green";
