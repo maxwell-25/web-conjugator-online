2ab5f9f43ab7fd0bc776d1af16ffcedc0c83c112
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-03-19 10:32:07 +0000

    tidied up code

--- js/header.js
+++ js/header.js
@@ -1,9 +1,7 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("nav-sidebar").style.width = "250px";
   }
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("nav-sidebar").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("nav-sidebar").style.width = "0";
+}
