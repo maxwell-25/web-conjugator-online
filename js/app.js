946a25cda63956479bf7e24422d53a78aef3c6df
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-03-20 18:14:11 +0000

    javascript app functionality progressed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,66 @@
+// APP-MENU
+
+// Get id of any clicked button in app-menu div to use as currentTense
+function selectTense(buttonId){
+    buttonId = buttonId || window.event;
+    buttonId = buttonId.target || buttonId.srcElement;
+    if (buttonId.nodeName === 'BUTTON'){
+        currentTense = buttonId.id;
+        console.log("current tense: " + currentTense);
+        newTenseDicts();
+        newTenseDict();
+        appView();
+    }
+}
+
+function newTenseDicts(){
+    if (currentTense == "pres"){
+        currentTenseDicts = [presSimpAr, presSimpEr, presSimpIr];
+        console.log("current dicts: " + currentTenseDicts);
+    } else if (currentTense == "pret"){
+        currentTenseDicts = [pretAr, pretEr, pretIr];
+    } else if (currentTense == "imperf"){
+        currentTenseDicts = [imperfAr, imperfEr, imperfIr];
+    } else if (currentTense == "fut"){
+        currentTenseDicts = [futAr, futEr, futIr];
+    } else if (currentTense == "cond"){
+        currentTenseDicts = [condAr, condEr, condIr];
+    } else if (currentTense == "ref"){
+        currentTenseDicts = [refAr, refEr, refIr];
+    } else if (currentTense == "presCon"){
+        currentTenseDicts = [presConAr, presConEr, presConIr];
+    } else if (currentTense == "perf"){
+        currentTenseDicts = [perfAr, perfEr, perfIr];
+    } else if (currentTense == "pluperf"){
+        currentTenseDicts = [pluperfAr, pluperfEr, pluperfIr];
+    } else if (currentTense == "presSubj"){
+        currentTenseDicts = [presSubjAr, presSubjEr, presSubjIr];
+    } else if (currentTense == "imperaPos"){
+        currentTenseDicts = [imperaPosAr, imperaPosEr, imperaPosIr];
+    } else if (currentTense == "imperaNeg"){
+        currentTenseDicts = [imperaNegAr, imperaNegEr, imperaNegIr];
+    }
+}
+
+// load new dict (-ar, -er, or -ir) from active list of CurrentTenseDicts ***AND REMOVE FROM LIST***
+// *** update table view and call function for first person to test ***
+function newTenseDict(){
+    currentTenseDict = currentTenseDicts[Math.floor(Math.random() * currentTenseDicts.length)];
+    console.log("current dict: " + currentTenseDict.title);
+    updateTableTitle();
+}
+
+function updateTableTitle(){
+    // console.log("current dict title: " + currentTenseDict.title)
+    document.getElementById("app-title").innerHTML=currentTenseDict.title;
+}
+
+function appView(){
+    document.getElementById("app-menu").style.display="none"
+    document.getElementById("app").style.display="block"
+}
+
+// APP-PRACTICE
 document.getElementById("yo-per").style.color="red";
 document.getElementById("yo-end").innerHTML="?"
 document.getElementById("yo-end").style.color="red";
@@ -10,4 +73,9 @@ function showAnswer(){
     document.getElementById("yo-end").style.color="green";
     document.getElementById("yo-ex").innerHTML="hablo";
     document.getElementById("yo-ex").style.color="green";
+}
+
+function back(){
+    document.getElementById("app-menu").style.display="grid"
+    document.getElementById("app").style.display="none"
 }
---
+++ js/tense_dicts.js
@@ -0,0 +1,341 @@
+var presSimpAr = {
+    "title": "Present simple (-ar)<br>(stem + ?)",
+    "yo": "-o",
+    "tú": "-as",
+    "él/ella": "-a",
+    "nosotros/nosotras": "-amos",
+    "vosotros/vosotras": "-áis",
+    "ellos/ellas": "-an",
+}
+
+var presSimpEr = {
+    "title": "Present simple (-er)<br>(stem + ?)",
+    "yo": "-o",
+    "tú": "-es",
+    "él/ella": "-e",
+    "nosotros/nosotras": "-emos",
+    "vosotros/vosotras": "-éis",
+    "ellos/ellas": "-en",
+}
+
+var presSimpIr = {
+    "title": "Present simple (-ir)<br>(stem + ?)",
+    "yo": "-o",
+    "tú": "-es",
+    "él/ella": "-e",
+    "nosotros/nosotras": "-imos",
+    "vosotros/vosotras": "-ís",
+    "ellos/ellas": "-en",
+}
+
+var pretAr = {
+    "title": "Preterite (-ar)<br>(stem + ?)",
+    "yo": "-é",
+    "tú": "-aste",
+    "él/ella": "-ó",
+    "nosotros/nosotras": "-amos",
+    "vosotros/vosotras": "-asteis",
+    "ellos/ellas": "-aron"
+}
+
+pretEr = {
+    "title": "Preterite (-er)<br>(stem + ?)",
+    "yo": "-í",
+    "tú": "-iste",
+    "él/ella": "-ió",
+    "nosotros/nosotras": "-imos",
+    "vosotros/vosotras": "-isteis",
+    "ellos/ellas": "-ieron"
+}
+
+pretIr = {
+    "title": "Preterite (-ir)<br>(stem + ?)",
+    "yo": "-í",
+    "tú": "-iste",
+    "él/ella": "-ió",
+    "nosotros/nosotras": "-imos",
+    "vosotros/vosotras": "-isteis",
+    "ellos/ellas": "-ieron"
+}
+
+imperfAr = {
+    "title": "Imperfect (-ar)<br>(stem + ?)",
+    "yo": "-aba",
+    "tú": "-abas",
+    "él/ella": "-aba",
+    "nosotros/nosotras": "-ábamos",
+    "vosotros/vosotras": "-abais",
+    "ellos/ellas": "-aban"
+}
+
+imperfEr = {
+    "title": "Imperfect (-er)<br>(stem + ?)",
+    "yo": "-ía",
+    "tú": "-ías",
+    "él/ella": "-ía",
+    "nosotros/nosotras": "-íamos",
+    "vosotros/vosotras": "-íais",
+    "ellos/ellas": "-ían"
+}
+
+imperfIr = {
+    "title": "Imperfect (-ir)<br>(stem + ?)",
+    "yo": "-ía",
+    "tú": "-ías",
+    "él/ella": "-ía",
+    "nosotros/nosotras": "-íamos",
+    "vosotros/vosotras": "-íais",
+    "ellos/ellas": "-ían"
+}
+
+futAr = {
+    "title": "Future (-ar)<br>(infinitive + ?)",
+    "yo": "-é",
+    "tú": "-ás",
+    "él/ella": "-á",
+    "nosotros/nosotras": "-emos",
+    "vosotros/vosotras": "-éis",
+    "ellos/ellas": "-án"
+}
+
+futEr = {
+    "title": "Future (-er)<br>(infinitive + ?)",
+    "yo": "-é",
+    "tú": "-ás",
+    "él/ella": "-á",
+    "nosotros/nosotras": "-emos",
+    "vosotros/vosotras": "-éis",
+    "ellos/ellas": "-án"
+}
+
+futIr = {
+    "title": "Future (-ir)<br>(infinitive + ?)",
+    "yo": "-é",
+    "tú": "-ás",
+    "él/ella": "-á",
+    "nosotros/nosotras": "-emos",
+    "vosotros/vosotras": "-éis",
+    "ellos/ellas": "-án"
+}
+
+condAr = {
+    "title": "Conditional (-ar)<br>(infinitive + ?)",
+    "yo": "-ía",
+    "tú": "-ías",
+    "él/ella": "-ía",
+    "nosotros/nosotras": "-íamos",
+    "vosotros/vosotras": "-íais",
+    "ellos/ellas": "-ían"
+}
+
+condEr = {
+    "title": "Conditional (-er)<br>(infinitive + ?)",
+    "yo": "-ía",
+    "tú": "-ías",
+    "él/ella": "-ía",
+    "nosotros/nosotras": "-íamos",
+    "vosotros/vosotras": "-íais",
+    "ellos/ellas": "-ían"
+}
+
+condIr = {
+    "title": "Conditional (-ir)<br>(infinitive + ?)",
+    "yo": "-ía",
+    "tú": "-ías",
+    "él/ella": "-ía",
+    "nosotros/nosotras": "-íamos",
+    "vosotros/vosotras": "-íais",
+    "ellos/ellas": "-ían"
+}
+
+refAr = {
+    "title": "Reflexive (-ar)<br>(? + verb)",
+    "yo": "me",
+    "tú": "te",
+    "él/ella": "se",
+    "nosotros/nosotras": "nos",
+    "vosotros/vosotras": "os",
+    "ellos/ellas": "se"
+}
+
+refEr = {
+    "title": "Reflexive (-er)<br>(? + verb)",
+    "yo": "me",
+    "tú": "te",
+    "él/ella": "se",
+    "nosotros/nosotras": "nos",
+    "vosotros/vosotras": "os",
+    "ellos/ellas": "se"
+}
+
+refIr = {
+    "title": "Reflexive (-ir)<br>(? + verb)",
+    "yo": "me",
+    "tú": "te",
+    "él/ella": "se",
+    "nosotros/nosotras": "nos",
+    "vosotros/vosotras": "os",
+    "ellos/ellas": "se"
+}
+
+presConAr = {
+    "title": "Present continuous (-ar)<br>(? -> stem + ??)",
+    "yo": "estoy/<br>-ando",
+    "tú": "estás/<br>-ando",
+    "él/ella": "está/<br>-ando",
+    "nosotros/nosotras": "estamos/<br>-ando",
+    "vosotros/vosotras": "estáis/<br>-ando",
+    "ellos/ellas": "estan/<br>-ando"
+}
+
+presConEr = {
+    "title": "Present continuous (-er)<br>(? -> stem + ??)",
+    "yo": "estoy/<br>-iendo",
+    "tú": "estás/<br>-iendo",
+    "él/ella": "está/<br>-iendo",
+    "nosotros/nosotras": "estamos/<br>-iendo",
+    "vosotros/vosotras": "estáis/<br>-iendo",
+    "ellos/ellas": "estan/<br>-iendo"
+}
+
+presConIr = {
+    "title": "Present continuous (-ir)<br>(? -> stem + ??)",
+    "yo": "estoy/<br>-iendo",
+    "tú": "estás/<br>-iendo",
+    "él/ella": "está/<br>-iendo",
+    "nosotros/nosotras": "estamos/<br>-iendo",
+    "vosotros/vosotras": "estáis/<br>-iendo",
+    "ellos/ellas": "estan/<br>-iendo"
+}
+
+perfAr = {
+    "title": "Perfect (-ar)<br>(? -> stem + ??)",
+    "yo": "he/<br>-ado",
+    "tú": "has/<br>-ado",
+    "él/ella": "ha/<br>-ado",
+    "nosotros/nosotras": "hemos/<br>-ado",
+    "vosotros/vosotras": "habéis/<br>-ado",
+    "ellos/ellas": "han/<br>-ado"
+}
+
+perfEr = {
+    "title": "Perfect (-er)<br>(? -> stem + ??)",
+    "yo": "he/<br>-ido",
+    "tú": "has/<br>-ido",
+    "él/ella": "ha/<br>-ido",
+    "nosotros/nosotras": "hemos/<br>-ido",
+    "vosotros/vosotras": "habéis/<br>-ido",
+    "ellos/ellas": "han/<br>-ado"
+}
+
+perfIr = {
+    "title": "Perfect (-ir)<br>(? -> stem + ??)",
+    "yo": "he/<br>-ido",
+    "tú": "has/<br>-ido",
+    "él/ella": "ha/<br>-ido",
+    "nosotros/nosotras": "hemos/<br>-ido",
+    "vosotros/vosotras": "habéis/<br>-ido",
+    "ellos/ellas": "han/<br>-ido"
+}
+
+pluperfAr = {
+    "title": "Pluperfect (-ar)<br>(? -> stem + ??)",
+    "yo": "había/<br>-ado",
+    "tú": "habías/<br>-ado",
+    "él/ella": "había/<br>-ado",
+    "nosotros/nosotras": "habíamos/<br>-ado",
+    "vosotros/vosotras": "habíais/<br>-ado",
+    "ellos/ellas": "habían/<br>-ado"
+}
+
+pluperfEr = {
+    "title": "Pluperfect (-er)<br>(? -> stem + ??)",
+    "yo": "había/<br>-ido",
+    "tú": "habías/<br>-ido",
+    "él/ella": "había/<br>-ido",
+    "nosotros/nosotras": "habíamos/<br>-ido",
+    "vosotros/vosotras": "habíais/<br>-ido",
+    "ellos/ellas": "habían/<br>-ido"
+}
+
+pluperfIr = {
+    "title": "Pluperfect (-ir)<br>(? -> stem + ??)",
+    "yo": "había/<br>-ido",
+    "tú": "habías/<br>-ido",
+    "él/ella": "había/<br>-ido",
+    "nosotros/nosotras": "habíamos/<br>-ido",
+    "vosotros/vosotras": "habíais/<br>-ido",
+    "ellos/ellas": "habían/<br>-ido"
+}
+
+presSubjAr = {
+    "title": "Present subjunctive (-ar)<br>(stem + ?)",
+    "yo": "-e",
+    "tú": "-es",
+    "él/ella": "-e",
+    "nosotros/nosotras": "-emos",
+    "vosotros/vosotras": "-éis",
+    "ellos/ellas": "-en",
+}
+
+presSubjEr = {
+    "title": "Present subjunctive (-er)<br>(stem + ?)",
+    "yo": "-a",
+    "tú": "-as",
+    "él/ella": "-a",
+    "nosotros/nosotras": "-amos",
+    "vosotros/vosotras": "-áis",
+    "ellos/ellas": "-an",
+}
+
+presSubjIr = {
+    "title": "Present subjunctive (-ir)<br>(stem + ?)",
+    "yo": "-a",
+    "tú": "-as",
+    "él/ella": "-a",
+    "nosotros/nosotras": "-amos",
+    "vosotros/vosotras": "-áis",
+    "ellos/ellas": "-an",
+}
+
+imperaPosAr = {
+    "title": "Positive imperative (-ar)<br>(stem + ?)",
+    "tú": "-a!",
+    "nosotros/nosotras": "-emos!",
+    "vosotros/vosotras": "-ad!"
+}
+
+imperaPosEr = {
+    "title": "Positive imperative (-er)<br>(stem + ?)",
+    "tú": "-e!",
+    "nosotros/nosotras": "-amos!",
+    "vosotros/vosotras": "-ed!"
+}
+
+imperaPosIr = {
+    "title": "Positive imperative (-ir)<br>(stem + ?)",
+    "tú": "-e!",
+    "nosotros/nosotras": "-amos!",
+    "vosotros/vosotras": "-id!"
+}
+
+imperaNegAr = {
+    "title": 'Negative imperative (-ar)<br>("No" -> stem + ?)',
+    "tú": "-es!",
+    "nosotros/nosotras": "-emos!",
+    "vosotros/vosotras": "-éis!"
+}
+
+imperaNegEr = {
+    "title": 'Negative imperative (-er)<br>("No" -> stem + ?)',
+    "tú": "-as!",
+    "nosotros/nosotras": "-amos!",
+    "vosotros/vosotras": "-áis!"
+}
+
+imperaNegIr = {
+    "title": 'Negative imperative (-ir)<br>("No" -> stem + ?)',
+    "tú": "-as!",
+    "nosotros/nosotras": "-amos!",
+    "vosotros/vosotras": "-áis!"
+}
