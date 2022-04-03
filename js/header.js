42da503d6e430cbb1b16d6f5bd0fa2fa64404efc
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-04-03 17:24:10 +0100

    tidied up code

--- js/header.js
+++ js/header.js
@@ -1,3 +1,7 @@
+// EVENT LISTENERS
+document.getElementById("nav-close-btn").addEventListener("click", closeNav);
+
+
 function openNav() {
     document.getElementById("nav-sidebar").style.width = "250px";
   }
