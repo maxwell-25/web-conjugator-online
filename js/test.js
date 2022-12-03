a5a5940785908a45ccca9dceed52de3641ac8c6e
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-12-03 18:39:59 +0000

    test page updated

--- js/test.js
+++ js/test.js
@@ -2,7 +2,8 @@
 
 const tenses = ["Present simple"];
 
-const persons = ["I", "you", "he/she", "we", "y'all", "they"];
+const persons = ["I"];
+// const persons = ["I", "you", "he/she", "we", "you all", "they"];
 
 const verbs = {
     "speak": "hablar",
@@ -15,6 +16,7 @@ const verbs = {
 let currentTense;
 let currentPerson;
 let currentVerb;
+let translation;
 
 // | EVENT LISTENERS
 // document.getElementById("test-show-ans").addEventListener("click", showAnswer);
@@ -35,6 +37,13 @@ function newVerb(){
     document.getElementById("tense").innerHTML=currentTense;
 
     document.getElementById("English").innerHTML=currentPerson + " " + currentVerb;
+
+    translation = verbs[currentVerb].slice(0, -2);
+
+    // CHANGE THIS TO ACCESS FROM TENSE-DICTS
+    if (currentPerson === "I"){
+        document.getElementById("Spanish").innerHTML=translation + "o";        
+    }
 }
 
 newVerb();
