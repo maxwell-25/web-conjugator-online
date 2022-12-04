6580c3bc1b8eb2ee248ff28ff0969ea3d94988e1
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-12-04 17:39:27 +0000

    test page updated

--- js/test.js
+++ js/test.js
@@ -1,16 +1,26 @@
 // | ARRAYS AND VARIABLES
 
-const verbs = {
-    hablar: "talk",
-    vivir: "live"
-}
+const arVerbs = {
+    hablar: "talk"
+};
 
-const tenses = [presAr, pretAr];
+const erVerbs = {
+    comer: "to eat"
+};
+
+const irVerbs = {
+    vivir: "live"
+};
+
+const tenses = [presAr, presIr, pretAr];
 
 const persons = {
     "yo": "I",
     "tú": "you",
-    "él/ella": "he/she"
+    "él/ella": "he/she",
+    "nosotros/nosotras": "we",
+    "vosotros/vosotras": "you all",
+    "ellos/ellas": "they"
 };
 
 let currentVerb;
@@ -25,24 +35,49 @@ document.getElementById("test-not-it").addEventListener("click", tryAgain);
 // | TEST
 
 function newTest(){
+    updateTense();
+    newVerb();
+    newPerson();
+    updateEnglish();
+}
 
-    currentVerb = Object.keys(verbs)[Math.floor(Math.random() * Object.keys(verbs).length)];
-    console.log("current verb: " + currentVerb);
-
+function updateTense(){
     currentTense = tenses[Math.floor(Math.random() * tenses.length)];
     console.log("current tense: " + currentTense.title);
     // console.log("currentTense: " + JSON.stringify(currentTense.title).slice(1, JSON.stringify(currentTense.title.indexOf("("))));
 
     document.getElementById("tense").innerHTML=JSON.stringify(currentTense.title).slice(1, JSON.stringify(currentTense.title.indexOf("(")));
     // I THINK I CAN MASSIVELY CUT DOWN THE CODE IN APP.JS (ALL THE "IF TENSE ===" STUFF) BY USING VARIABLES IN MY CONST ARRAYS RATHER THAN STRINGS, THEN USING STRINGIFY TO CALL THEM)
-
-    currentPerson = Object.keys(persons)[Math.floor(Math.random() * Object.keys(persons).length)];
-    console.log("currentPerson: " + currentPerson);
-
-    document.getElementById("english").innerHTML=persons[currentPerson] + " " + verbs[currentVerb];
 }
 
-function revealEnding(){
+function newVerb(){
+    if (String(currentTense).includes("(-ar)")){
+        currentVerb = Object.keys(arVerbs)[Math.floor(Math.random() * Object.keys(arVerbs).length)];
+        console.log("current verb: " + currentVerb);
+    } else if (String(currentTense).includes("(-er")){
+        currentVerb = Object.keys(erVerbs)[Math.floor(Math.random() * Object.keys(erVerbs).length)];
+        console.log("current verb: " + currentVerb);
+    } else if (String(currentTense).includes("(-ir")){
+        currentVerb = Object.keys(irVerbs)[Math.floor(Math.random() * Object.keys(irVerbs).length)];
+        console.log("current verb: " + currentVerb);
+    }
+}
+
+function newPerson(){
+    currentPerson = Object.keys(persons)[Math.floor(Math.random() * Object.keys(persons).length)];
+    console.log("currentPerson: " + currentPerson);
+}
+
+
+function updateEnglish(){
+    if (currentPerson == "él/ella") {
+        document.getElementById("english").innerHTML=persons[currentPerson] + " " + arVerbs[currentVerb] + "s";
+    } else {
+        document.getElementById("english").innerHTML=persons[currentPerson] + " " + currentVerb;
+    }
+}
+
+function newEnding(){
     document.getElementById("spanish").innerHTML=currentVerb.slice(0,-2) + currentTense[currentPerson].slice(1);
 }
 
@@ -65,7 +100,7 @@ function resetTable(){
 // | BUTTON FUNCTIONS
 
 function showAnswer() {
-    revealEnding();
+    newEnding();
     answerView()
 }
 
