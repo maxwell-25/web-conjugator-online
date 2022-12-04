1a255a6271f0403b9c03211eb6c6a0cf8ffec390
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-12-04 14:23:16 +0000

    test page updated

--- js/test.js
+++ js/test.js
@@ -1,5 +1,10 @@
 // | ARRAYS AND VARIABLES
 
+const verbs = {
+    hablar: "talk",
+    vivir: "live"
+}
+
 const tenses = [presAr, pretAr];
 
 const persons = {
@@ -7,26 +12,25 @@ const persons = {
     "tú": "you",
     "él/ella": "he/she"
 };
-// const persons = ["yo", "tú", "él/ella", "nosotros/nosotras", "vosotros/vosotras", "ellos/ellas"];
 
-const verbs = {
-    hablar: "talk",
-    vivir: "live"
-}
-
+let currentVerb;
 let currentTense;
 let currentPerson;
-let currentVerb;
-let englishPerson;
 
 // | EVENT LISTENERS
-// document.getElementById("test-show-ans").addEventListener("click", showAnswer);
+document.getElementById("test-show-ans").addEventListener("click", showAnswer);
+document.getElementById("test-got-it").addEventListener("click", gotIt);
+document.getElementById("test-not-it").addEventListener("click", tryAgain);
 
 // | TEST
 
 function newTest(){
+
+    currentVerb = Object.keys(verbs)[Math.floor(Math.random() * Object.keys(verbs).length)];
+    console.log("current verb: " + currentVerb);
+
     currentTense = tenses[Math.floor(Math.random() * tenses.length)];
-    console.log("current tense: " + currentTense);
+    console.log("current tense: " + currentTense.title);
     // console.log("currentTense: " + JSON.stringify(currentTense.title).slice(1, JSON.stringify(currentTense.title.indexOf("("))));
 
     document.getElementById("tense").innerHTML=JSON.stringify(currentTense.title).slice(1, JSON.stringify(currentTense.title.indexOf("(")));
@@ -35,13 +39,46 @@ function newTest(){
     currentPerson = Object.keys(persons)[Math.floor(Math.random() * Object.keys(persons).length)];
     console.log("currentPerson: " + currentPerson);
 
-    
-    currentVerb = Object.keys(verbs)[Math.floor(Math.random() * Object.keys(verbs).length)];
-    console.log("current verb: " + currentVerb);
-
     document.getElementById("English").innerHTML=persons[currentPerson] + " " + verbs[currentVerb];
+}
 
+function revealEnding(){
     document.getElementById("Spanish").innerHTML=currentVerb.slice(0,-2) + currentTense[currentPerson].slice(1);
+}
+
+function questionView(){
+    document.getElementById("test-show-ans").style.display="inline-block";
+    document.getElementById("test-got-it").style.display="none";
+    document.getElementById("test-not-it").style.display="none";
+}
+
+function answerView(){
+    document.getElementById("test-show-ans").style.display="none";
+    document.getElementById("test-got-it").style.display="inline-block";
+    document.getElementById("test-not-it").style.display="inline-block";
+}
+
+function resetTable(){
+    document.getElementById("Spanish").innerHTML="?";
+}
+
+// | BUTTON FUNCTIONS
+
+function showAnswer() {
+    revealEnding();
+    answerView()
+}
+
+function gotIt(){
+    questionView();
+    newTest();
+    resetTable();
+}
+
+function tryAgain(){
+    questionView();
+    newTest();
+    resetTable();
 }
 
 newTest();
