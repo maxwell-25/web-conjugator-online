89d532f29dd6e802990b10d84dcba92ed032fa20
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-12-04 01:10:16 +0000

    test page updated

--- js/test.js
+++ js/test.js
@@ -1,22 +1,24 @@
 // | ARRAYS AND VARIABLES
 
-const tenses = ["Present simple"];
+const tenses = [presAr, pretAr];
 
-const persons = ["I"];
-// const persons = ["I", "you", "he/she", "we", "you all", "they"];
+const persons = {
+    "yo": "I"
+};
+// const persons = ["yo", "tú", "él/ella", "nosotros/nosotras", "vosotros/vosotras", "ellos/ellas"];
 
 const verbs = {
-    "speak": "hablar",
-    "take": "tomar",
-    "live": "vivir",
-    "watch": "mirar",
-    "work": "trabajar"
+    "hablar": "speak",
+    "tomar": "take",
+    "vivir": "live",
+    "mirar": "watch",
+    "trabajar": "work"
 }
 
 let currentTense;
 let currentPerson;
 let currentVerb;
-let translation;
+let englishPerson;
 
 // | EVENT LISTENERS
 // document.getElementById("test-show-ans").addEventListener("click", showAnswer);
@@ -25,25 +27,19 @@ let translation;
 
 function newVerb(){
     currentTense = tenses[Math.floor(Math.random() * tenses.length)];
-    console.log(currentTense);
+    // console.log(currentTense);
+    // console.log("currentTense: " + JSON.stringify(currentTense.title).slice(1, JSON.stringify(currentTense.title.indexOf("("))));
 
-    currentPerson = persons[Math.floor(Math.random() * persons.length)];
-    console.log(currentPerson);
+    document.getElementById("tense").innerHTML=JSON.stringify(currentTense.title).slice(1, JSON.stringify(currentTense.title.indexOf("(")));
+    // I THINK I CAN MASSIVELY CUT DOWN THE CODE IN APP.JS (ALL THE "IF TENSE ===" STUFF) BY USING VARIABLES IN MY CONST ARRAYS RATHER THAN STRINGS, THEN USING THIS STRINGIFY METHOD TO CALL THEM)
+
+    currentPerson = Object.keys(persons)[Math.floor(Math.random() * Object.keys(persons).length)];
+    console.log("currentPerson: " + currentPerson);
 
     currentVerb = Object.keys(verbs)[Math.floor(Math.random() * Object.keys(verbs).length)];
     console.log(currentVerb);
 
-
-    document.getElementById("tense").innerHTML=currentTense;
-
-    document.getElementById("English").innerHTML=currentPerson + " " + currentVerb;
-
-    translation = verbs[currentVerb].slice(0, -2);
-
-    // CHANGE THIS TO ACCESS FROM TENSE-DICTS
-    if (currentPerson === "I"){
-        document.getElementById("Spanish").innerHTML=translation + "o";        
-    }
+    document.getElementById("English").innerHTML=persons[currentPerson] + " " + verbs[currentVerb];
 }
 
 newVerb();
