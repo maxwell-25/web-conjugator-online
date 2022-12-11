266984da3a0d515621bc34958656a72b035d5c22
Max Williams <93284907+maxwell-25@users.noreply.github.com>
2022-12-11 13:34:06 +0000

    test page updated

--- js/test.js
+++ js/test.js
@@ -14,6 +14,19 @@ const irVerbs = {
     vivir: "To live"
 };
 
+const reflexiveArVerbs = {
+    lavar: "To wash"
+};
+
+const reflexiveErVerbs = {
+    atreverse: "To dare"
+};
+
+const reflexiveIrVerbs = {
+    aburrir: "To get bored"
+};
+
+
 const tenses = [
     presAr, presEr, presIr,
     pretAr, pretEr, pretIr,
@@ -28,6 +41,23 @@ const tenses = [
     imperaPosAr, imperaPosEr, imperaPosIr,
     imperaNegAr, imperaNegEr, imperaNegIr
 ];
+
+// const tenses = [
+//     presAr,
+//     pretAr,
+//     imperfAr,
+//     futAr,
+//     condAr,
+//     refAr,
+//     presConAr,
+//     perfAr,
+//     pluperfAr,
+//     presSubjAr,
+//     imperaPosAr,
+//     imperaNegAr,
+// ];
+
+const reflexiveTenses = [refAr, refEr, refIr];
 
 const persons = {
     "yo": "I",
@@ -65,7 +95,15 @@ function updateTense(){
 }
 
 function newVerbType(){
-    if (currentTense.title.includes("(-ar)")){
+    if (currentTense.title.includes("Reflexive")){
+        if (currentTense.title.includes("(-ar)")){
+            currentVerbType = reflexiveArVerbs;
+        } else if (currentTense.title.includes("(-er)")){
+            currentVerbType = reflexiveErVerbs;
+        } else if (currentTense.title.includes("(-ir)")){
+            currentVerbType = reflexiveIrVerbs;
+        }
+    } else if (currentTense.title.includes("(-ar)")){
         currentVerbType = arVerbs;
         console.log("currentVerbType:", currentVerbType);
     } else if (currentTense.title.includes("(-er)")){
@@ -83,17 +121,45 @@ function newVerb(){
 }
 
 function newPerson(){
-    currentPerson = Object.keys(persons)[Math.floor(Math.random() * Object.keys(persons).length)];
+    if (currentTense.title.includes("imperative")){
+        console.log("test");
+        const imperativePersons = {
+        "tú": "you",
+        "nosotros/nosotras": "we",
+        "vosotros/vosotras": "you all"
+        };
+        currentPerson = Object.keys(imperativePersons)[Math.floor(Math.random() * Object.keys(imperativePersons).length)]
+     } else{currentPerson = Object.keys(persons)[Math.floor(Math.random() * Object.keys(persons).length)];
     console.log("currentPerson: " + currentPerson);
+    }
 }
-
 
 function updateEnglish(){
     document.getElementById("english").innerHTML=currentVerbType[currentVerb] + "<br />" + "(" + persons[currentPerson] + ")";
 }
 
-function newEnding(){
-    document.getElementById("spanish").innerHTML=currentVerb.slice(0,-2) + currentTense[currentPerson].slice(1);
+function newAnswer(){
+    if(currentTense.title.includes("Present continuous") || currentTense.title.includes("Perfect") || currentTense.title.includes("Pluperfect")){
+        document.getElementById("spanish").innerHTML = currentTense[currentPerson].split("/")[0] + " " + currentVerb.slice(0,-2) + currentTense[currentPerson].split("-")[1];
+    } else if (currentTense.title.includes("Future") || currentTense.title.includes("Conditional")) {
+        document.getElementById("spanish").innerHTML = currentVerb + currentTense[currentPerson].slice(1);
+    } else if(currentTense.title.includes("Reflexive")){
+        if(currentTense.title.includes("-ar")){
+            const reflexiveEnding = presAr;
+            console.log("refEnding:", reflexiveEnding[currentPerson]);
+            document.getElementById("spanish").innerHTML = currentTense[currentPerson] + " " + currentVerb.slice(0,-2) + reflexiveEnding[currentPerson].slice(1);
+        } else if(currentTense.title.includes("-er")){
+            const reflexiveEnding = presEr;
+            console.log("refEnding:", reflexiveEnding[currentPerson]);
+            document.getElementById("spanish").innerHTML = currentTense[currentPerson] + " " + currentVerb.slice(0,-2) + reflexiveEnding[currentPerson].slice(1);
+        } else if(currentTense.title.includes("-ir")){
+            const reflexiveEnding = presIr;
+            console.log("refEnding:", reflexiveEnding[currentPerson]);
+            document.getElementById("spanish").innerHTML = currentTense[currentPerson] + " " + currentVerb.slice(0,-2) + reflexiveEnding[currentPerson].slice(1);
+        }
+    } else {
+        document.getElementById("spanish").innerHTML=currentVerb.slice(0,-2) + currentTense[currentPerson].slice(1);
+    }
 }
 
 function questionView(){
@@ -114,7 +180,7 @@ function resetTable(){
 
 // | BUTTON FUNCTIONS
 function showAnswer() {
-    newEnding();
+    newAnswer();
     answerView()
 }
 
